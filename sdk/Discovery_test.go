7274b00aa752da1e5841f067bb256a826a22bb19
Gábor Nyíri <halacs@users.noreply.github.com>
2023-12-09 20:52:45 +0100

    create separated test file for tests contacting real devices

--- sdk/Discovery_test.go
+++ sdk/Discovery_test.go
@@ -1,35 +1,11 @@
 package sdk
 
 import (
-	"context"
 	"encoding/xml"
 	"fmt"
 	"reflect"
 	"testing"
-	"time"
 )
-
-func TestDiscoveryOnRealGateway(t *testing.T) {
-	t.SkipNow()
-
-	ctx := context.Background()
-	discovery := NewDiscovery(ctx, func(gateway Gateway) {
-		t.Logf("Response received: %+v\n", gateway)
-	})
-
-	t.Logf("Start discovery...\n")
-	discovery.Start()
-
-	t.Logf("Waiting few second for responses...\n")
-	time.Sleep(time.Second * 20)
-
-	list := discovery.GetList()
-	t.Logf("list: %+v\n", list)
-
-	t.Logf("Stop disovery...\n")
-	discovery.Stop()
-	t.Logf("Terminated\n")
-}
 
 func TestUnmarshalDiscoveryResponseXml(t *testing.T) {
 	response := "<LogicBox swVersion=\"2.5.0\" hwVersion=\"1.0.0\" mac=\"54:10:EC:85:28:BB\" protocol=\"MCP V3.0\"/>"
